a49335e7078158296bdb1d29d12e2448b0a8866e
Gabrielle <eastongabrielle@gmail.com>
2021-06-17 19:48:45 -0400

    reversed string solutions added

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -5,7 +5,40 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
+//O(n)
+// function reverse(str) {
+//   let reversed = '';
+//   for (i = 0; i < str.length; i += 1){
+//     reversed = str[i] + reversed;
+//   }
+//   return reversed;
+// }
+// console.log(reverse("Apple!"))
 
-function reverse(str) {}
+// function reverse(str) {
+//   return str.split('').reverse().join('')
+// }
+// console.log(reverse("Apple!"))
+
+// function reverse(str) {
+//   let reversed = '';
+  
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
+// console.log(reverse("Apple!"))
+
+function reverse(str) {
+  return str.split('').reduce((reversed, character) =>
+    character + reversed
+    , '');
+}
+console.log(reverse("Apple!"))
+
+
+
+
 
 module.exports = reverse;
