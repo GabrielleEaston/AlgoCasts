802b4d95ff27204fbb894a19d7fe32cfdf89a74c
Gabrielle <eastongabrielle@gmail.com>
2021-06-18 18:08:38 -0400

    maxchar solved

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,25 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
-
-module.exports = { Node, LinkedList };
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+    
+  }
+}
+const node1 = new Node("hello");
+const list = new LinkedList();
+console.log(node1);
+console.log(linked1);
+linked1.insertFirst("hello");
+//module.exports = { Node, LinkedList };
