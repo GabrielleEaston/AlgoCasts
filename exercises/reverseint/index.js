22fc98e4d63b960f850fdbd8a64f105f886fb000
Gabrielle <eastongabrielle@gmail.com>
2021-06-17 20:41:11 -0400

    reverse int solved

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -7,7 +7,15 @@
 //   reverseInt(500) === 5
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
-
-function reverseInt(n) {}
-
+//parseInt
+//Math.sign
+//toString
+function reverseInt(n) {
+  const rev = n.toString().split('').reverse().join('');
+  // if (n < 0) {
+  //   return parseInt(rev) * -1;
+  // }
+  return parseInt(rev) * Math.sign(n);
+}
+console.log(reverseInt(500));
 module.exports = reverseInt;
