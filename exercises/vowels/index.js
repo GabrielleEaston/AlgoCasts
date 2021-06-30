a2305068707408952e5f5ceaa6d567ad0d489e2c
Gabrielle <eastongabrielle@gmail.com>
2021-06-30 17:58:11 -0400

    vowels solutions

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,23 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//   let counter = 0;
+//   const checker = ['a', 'e', 'i', 'o', 'u']
+//   for (let letter of str.toLowerCase()) {
+//     // if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
+//     //   counter += 1;
+//     // }
+//     if (checker.includes(letter)) {
+//       counter++;
+//     }
+//   }
+//   return counter;
+// }
 
+function vowels(str) {
+  const result = str.match(/[aeiou]/ig);
+  return result ? result.length : 0;
+}
+console.log(vowels("Why do you ask?"))
 module.exports = vowels;
